f7998c7b6bb1bc41dc02623f0b78c2064571a4ee
Johan Pelkonen <johanpelkonen@dhcp-84-251.eduroam.aalto.fi>
2017-11-29 12:50:03 +0200

    Box replaced with a new version

---
+++ src/Box2D_v2.1.2/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Testbed/Framework/Main.cpp" "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Framework/Main.o"
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Testbed/Framework/Render.cpp" "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Framework/Render.o"
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Testbed/Framework/Test.cpp" "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Framework/Test.o"
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Testbed/Tests/TestEntries.cpp" "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Tests/TestEntries.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FREEGLUT_EXPORTS"
+  "FREEGLUT_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk/System/Library/Frameworks/OpenGL.framework"
+  "../"
+  "/usr/X11/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake"
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/DependInfo.cmake"
+  "/Users/johanpelkonen/Downloads/Box2D_v2.1.2/Box2D/Build/glui/CMakeFiles/glui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
