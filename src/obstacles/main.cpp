af36c434fce977371c293a06e6922cc9b7657199
Sauli Sjögren <sjogres1@aalto.fi>
2017-12-01 14:50:57 +0200

    mainfile and obstacle folder changes

--- src/obstacles/main.cpp
+++ src/obstacles/main.cpp
@@ -17,7 +17,7 @@ b2Vec2 convertWorldToScreen( const b2Vec2& v) {
 int main(int argc, char** argv)
 {
   //Setup SFML
-  sf::RenderWindow window( sf::VideoMode( WWIDTH,WHEIGHT), "Obstacle test");
+  sf::RenderWindow window( sf::VideoMode( WWIDTH,WHEIGHT), "Obsstacle test");
 
   B2_NOT_USED(argc);
   B2_NOT_USED(argv);
