d86d191f443c733c27e1e6a387c909e800ff7838
Sauli Sjögren <sjogres1@aalto.fi>
2017-12-08 09:46:56 +0200

    more obstacle classes

---
+++ src/obstacles/obstacle_coin.hpp
@@ -0,0 +1,14 @@
+#ifndef OBSTACLE_COIN_HPP
+#define OBSTACLE_COIN_HPP
+
+#include "obstacle_coin.cpp"
+
+/* Obstacle coin class. Tells all the information about coins that are on the map */
+
+
+class obstacle_coin
+{
+
+public:
+	
+}
