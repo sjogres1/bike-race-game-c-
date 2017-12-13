39fa8e94736391fbb37f362df87b3359f88fce53
Ilkka Saarnilehto <ilkka.saarnilehto@aalto.fi>
2017-12-13 13:30:01 +0200

    screen logic implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <Box2D/Box2D.h>
-
 #include "Game.hpp"
+#include "MainMenu.hpp"
+#include "Screens.hpp"
+#include <vector>
 
-int main(void) {
-    
+int main(){
+
+    std::vector<Screen*> screens;
+    int screen = 0;
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    //TODO:fetch screen size from a separate file to unify
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Hillside Racing", sf::Style::Default, settings);
+
+    MainMenu mm;
+    screens.push_back (&mm);
+
     Game g;
-    g.open();
-    
-    return 0;
+    screens.push_back(&g);
+
+    while (screen >= 0) {
+        screen = screens[screen]->open(window);
+        if(screen == GAMESTATE_GAME) {g.open(window);}
+   }
 }
