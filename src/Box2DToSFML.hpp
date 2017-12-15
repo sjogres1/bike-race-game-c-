c0bf2267c48fd6701080a87fd60e3d6252577c09
Sauli Sjögren <sjogres1@aalto.fi>
2017-12-15 12:47:18 +0200

    cleaning useless commets away

--- src/Box2DToSFML.hpp
+++ src/Box2DToSFML.hpp
@@ -11,27 +11,10 @@ namespace {
 }
  namespace B2toSFRenderer {
     sf::ConvexShape PolygonToSFConvex(b2PolygonShape& polygonShape);
-   /* {
-        sf::ConvexShape shape;
-        int32 vcount = polygonShape.GetVertexCount();
-        shape.setPointCount(vcount);
-        for (int32 i = 0; i < vcount; i++) {
-            const b2Vec2 v = polygonShape.GetVertex(i);
-            shape.setPoint(i, sf::Vector2f(v.x*Pix_Per_M,
-                                           v.y*Pix_Per_M*(-1)));
-        }
-        return shape;
-    }*/
+   
    
     sf::CircleShape CircleToSFCircle(b2CircleShape& circleShape);
-   /* {
-        sf::CircleShape shape;
-        float32 radius = circleShape.m_radius;
-        shape.setPointCount(16);
-        shape.setRadius(radius * Pix_Per_M);
-        shape.setOrigin(radius * Pix_Per_M, radius * Pix_Per_M);
-        return shape;
-    }*/
+   
  }
  
 #endif
