3a62dd52c60d8353d19a9a75e2f9ce8274544b70
Sauli Sjögren <sjogres1@aalto.fi>
2017-12-15 11:46:49 +0200

    making it look smarter

--- src/Coin.hpp
+++ src/Coin.hpp
@@ -23,50 +23,16 @@ class Coin : public GameObject
     //protected:
     //Player* player;
     public:
-    Coin(b2World* world, Player* player, double x, double y) { //  Player* p, removed
-        m_contacting = false;
-        collected = false;
-        //player = p;
-        player1 = player;
-        
-        b2CircleShape circle;
-        circle.m_radius = 1.0f;
-        coin = B2toSFRenderer::CircleToSFCircle(circle);
-        cointexture.loadFromFile("coin.png");
-        cointexture.setSmooth(true);
-        coin.setTexture(&cointexture, true);
-        
-        b2FixtureDef fd;
-        fd.shape = &circle;
-        fd.density = 1.1f;
-        fd.isSensor = true;
-        
-        b2BodyDef bd;
-        bd.position.Set(x,y);
-        m_coin = world->CreateBody(&bd);
-        m_coin->CreateFixture(&fd);
-        m_coin->SetUserData(this);
-    }
+    Coin(b2World* world, Player* player, double x, double y);
     
-    void render(sf::RenderTarget &rt) const
+    
+    
+    
+    void update(); 
+
+        void render(sf::RenderTarget &rt) const
     {
         if(!collected) {rt.draw(coin);}
-        
-    }
-    
-    
-    void update()
-    {
-        if (m_contacting) {
-            m_coin->GetWorld()->DestroyBody(m_coin);
-            collected = true;
-            player1->increasePoints(2);
-            
-        }
-        
-        coin.setOrigin(coin.getRadius(), coin.getRadius());
-        coin.setPosition((m_coin->GetPosition().x )*Pix_Per_M,
-                (m_coin->GetPosition().y)*Pix_Per_M*(-1));
         
     }
     
